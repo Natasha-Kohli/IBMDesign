c2cf43748babfe4e1bb5f3d4584b937b0aeddeeb
NatashaKohli <natashakohli@ucla.edu>
2019-05-14 23:23:50 -0700

    Updated location to NYC

--- IBMReact/App.js
+++ IBMReact/App.js
@@ -1,28 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View, TextInput } from 'react-native';
 import { MapView } from 'expo';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <MapView
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
-    );
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    // this.state = { 
+    // };
   }
+
+  render() {
+        return (
+          <MapView
+            style={{ flex: 1 }}
+            initialRegion={{
+              latitude: 40.7128,
+              longitude: -74.0060,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
+        );
+      }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+
