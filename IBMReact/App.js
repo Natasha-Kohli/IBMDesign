c045f4ee5b9c26345792864739a61b360516095c
NatashaKohli <natashakohli@ucla.edu>
2019-06-03 13:05:01 -0700

    Pulled map component out of app

--- IBMReact/App.js
+++ IBMReact/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { Alert, StyleSheet, View, TextInput, Text, Button, TimePickerAndroid, FlatList } from 'react-native';
-import { MapView } from 'expo';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import {createStackNavigator, createAppContainer} from 'react-navigation';
 import { List, ListItem } from 'react-native-elements'
 
-import './global.js'
+import './global.js';
+import Map from './Components/map.js';
 
 const styles = StyleSheet.create({
-  mapFlex: {
-    display: "flex",
-    height: "100%"
-  },
   container: {
     display: "flex",
     alignItems: "stretch",
@@ -32,7 +28,6 @@ const styles = StyleSheet.create({
   },
   calloutSearch: {
     margin: 5,
-    // marginTop: 30,
     borderColor: "black",
     borderRadius: 5,
     backgroundColor: "white"
@@ -48,300 +43,6 @@ const styles = StyleSheet.create({
   }
 });
 
-var mapStyle = [
-  {
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#212121"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.icon",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#212121"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.country",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#9e9e9e"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.land_parcel",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.land_parcel",
-    "elementType": "labels",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "administrative.locality",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#bdbdbd"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.business",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#181818"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#616161"
-      }
-    ]
-  },
-  {
-    "featureType": "poi.park",
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {
-        "color": "#1b1b1b"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#ff00ff"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "geometry.fill",
-    "stylers": [
-      {
-        "color": "#ff00ff"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "geometry.stroke",
-    "stylers": [
-      {
-        "color": "#ff00ff"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "labels.icon",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#8a8a8a"
-      }
-    ]
-  },
-  {
-    "featureType": "road.arterial",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#ff00ff"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#3c3c3c"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "geometry.fill",
-    "stylers": [
-      {
-        "color": "#ff00ff"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway.controlled_access",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#ff00ff"
-      }
-    ]
-  },
-  {
-    "featureType": "road.highway.controlled_access",
-    "elementType": "geometry.fill",
-    "stylers": [
-      {
-        "color": "#ff00ff"
-      }
-    ]
-  },
-  {
-    "featureType": "road.local",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#ff00ff"
-      }
-    ]
-  },
-  {
-    "featureType": "road.local",
-    "elementType": "labels",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "road.local",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#616161"
-      }
-    ]
-  },
-  {
-    "featureType": "transit",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "transit",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#757575"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "color": "#000000"
-      }
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {
-        "color": "#3d3d3d"
-      }
-    ]
-  }
-]
-
-
-var region = {
-  latitude: 40.7128,
-  longitude: -74.0060,
-  latitudeDelta: 0.0922,
-  longitudeDelta: 0.0421,
-};
-
-var serverURL = 'https://feeds.citibikenyc.com/stations/stations.json'
-
 class SearchScreen extends React.Component {
   static navigationOptions = {
     title: 'Catch-A-Ride',
@@ -350,9 +51,6 @@ class SearchScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      isLoading: true,
-      markers: [],
-      displayMarkers: false,
       timeHour: 12,
       timeMinute: 0,
       startText: "Enter Pickup Location",
@@ -425,45 +123,7 @@ class SearchScreen extends React.Component {
     navigate('GoogleLocations')
   } 
 
-  renderMarkers() {
-    if ( !this.state.displayMarkers ) return null;
-    return this.state.isLoading
-      ? null
-      : this.state.markers.map((marker, index) => {
-          const coords = {
-            latitude: marker.latitude,
-            longitude: marker.longitude
-          };
-
-          const metadata = `Status: ${marker.statusValue}`;
-
-          return (
-            <MapView.Marker
-              key={index}
-              coordinate={coords}
-              title={marker.stationName}
-              description={metadata}
-            />
-          );
-        });
-  }
-
   componentDidMount() {
-    this.fetchData();
-  }
-
-  fetchData() {
-    fetch(serverURL)
-      .then((response) => response.json())
-      .then((responseJson) => {
-        this.setState({ 
-          isLoading: false,
-          markers: responseJson.stationBeanList, 
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
   }
 
   render() {
@@ -504,14 +164,7 @@ class SearchScreen extends React.Component {
             color="#FF1493"
             accessibilityLabel="Start your search"
           />
-        <MapView
-          style={styles.mapFlex}
-          provder="google"
-          initialRegion={region}
-          customMapStyle={mapStyle}
-        >
-          {this.renderMarkers()}
-        </MapView>
+        <Map />
       </View>
     );
   }
