34e4cf9c147a45133206463ec1885376db20fb20
Amit Mondal <>
2019-06-04 14:34:53 -0700

    Fixed return to be dict and lat/lon instead of cluster

--- server/environment/recommend.py
+++ server/environment/recommend.py
@@ -41,28 +41,39 @@ def get_adj_clusters(lat, lon, radius, cursor):
     where ST_DWithin(geom, ST_MakePoint(%(lon)s, %(lat)s)::geography, %(radius)s)
     group by clusterx, clustery"""
 
-    print(cursor.mogrify(query, {'lat' : lat, 'lon' : lon, 'radius' : radius}))
-
     cursor.execute(query, {'lat' : lat, 'lon' : lon, 'radius' : radius})
     rows = cursor.fetchall()
     return rows
-    
+
+def get_random_point(clusterx, clustery, cursor):
+    query="""select lat, lon from ride
+    where clusterx=%(clusterx)s and clustery = %(clustery)s
+    limit 1"""
+    cursor.execute(query, {'clusterx' : clusterx, 'clustery' : clustery})
+    rows = cursor.fetchall()
+    return rows[0]
 
 def get_chunk_idx(hour, minute):
     return 4 * hour + minute // 15
 
-def get_row_with_pred(row, clf, day, month, chunkIdx):
-    return (row[0], row[1], clf.predict([[row[0], row[1], day, month, chunkIdx]]).tolist())
+def get_row_with_pred(row, clf, day, month, chunkIdx, cursor):
+    point = get_random_point(row[0], row[1], cursor)
+    return {'lat' : point[0],
+            'lon' : point[1],
+            'nrides' : clf.predict([[row[0], row[1], day, month, chunkIdx]]).tolist()[0]}
 
-def get_ranked_clusters(adj_clusters, day, month, chunkIdx, limit):
+def get_ranked_clusters(adj_clusters, day, month, chunkIdx, limit, cursor):
     clf = get_clf()
-    clusters = [get_row_with_pred(row, clf, day, month, chunkIdx) for row in adj_clusters]
-    clusters.sort(reverse=True, key=(lambda row : row[2]))
-    return clusters[:limit]
+    clusters = [get_row_with_pred(row, clf, day, month, chunkIdx, cursor) for row in adj_clusters]
+    clusters.sort(reverse=True, key=(lambda row : row['nrides']))
+    if limit > 0:
+        return clusters[:limit]
+    else:
+        return clusters
 
 def recommend(lat, lon, radius, nrows, day, month, hour, minute):    
     try:
-        #REPLACE WITH YOUR OWN DETAILS
+        # REPLACE WITH YOUR OWN DETAILS
         connection = psycopg2.connect(user='amit',
                                       host='127.0.0.1',
                                       port='5432',
@@ -70,7 +81,7 @@ def recommend(lat, lon, radius, nrows, day, month, hour, minute):
         cursor = connection.cursor()
         adj_clusters = get_adj_clusters(lat, lon, radius, cursor)
         chunkIdx = get_chunk_idx(hour, minute)
-        return get_ranked_clusters(adj_clusters, day, month, chunkIdx, nrows)
+        return get_ranked_clusters(adj_clusters, day, month, chunkIdx, nrows, cursor)
         
     except (Exception, psycopg2.Error) as error:
         print('Error while connecting to DB')
