4f9b7ffb4b48ae54f084e8ad3f02111ab1d57607
Amit Mondal <>
2019-06-04 18:46:46 -0700

    new schema loading script

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,10 +2,26 @@
 DROP TABLE IF EXISTS ride;
 
 CREATE TABLE ride(
+       id int,
        time timestamp,
-       lat float(5),
-       lon float(5),
+       point text,
        base char(6),
-       clusterx int default 0,
-       clustery int default 0
+       clusterx int,
+       clustery int,
+       week int,
+       month int
 );
+
+\copy ride (id, time, point, base, clusterx, clustery, week, month) FROM '~/Downloads/grid_chunked.csv' WITH DELIMITER ',' QUOTE '"' CSV HEADER;
+
+alter table ride add column geog geography(POINT, 4326);
+update ride set geog = ST_geogFromText(point);
+
+alter table ride
+drop column id,
+drop column point,
+drop column base;
+
+CREATE INDEX ride_idx
+  ON ride
+  USING GIST (geog);
