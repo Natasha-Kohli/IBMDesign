d271dd047bc9028173355f5703fa649a3d3de23a
Amit Mondal <>
2019-06-05 20:44:22 -0700

    Server updated with new db, geotagging, new format, etc.

--- sql/schema.sql
+++ sql/schema.sql
@@ -4,24 +4,23 @@ DROP TABLE IF EXISTS ride;
 CREATE TABLE ride(
        id int,
        time timestamp,
-       point text,
-       base char(6),
+       lat float(10),
+       lon float(10),
+       day_week int,
+       day_month int,
+       chunk_idx int,
        clusterx int,
-       clustery int,
-       week int,
-       month int
+       clustery int
 );
 
-\copy ride (id, time, point, base, clusterx, clustery, week, month) FROM '~/Downloads/grid_chunked.csv' WITH DELIMITER ',' QUOTE '"' CSV HEADER;
+\copy ride FROM '~/Downloads/db_all_aug14box.csv' WITH DELIMITER ',' QUOTE '"' CSV HEADER;
 
-alter table ride add column geog geography(POINT, 4326);
-update ride set geog = ST_geogFromText(point);
+SELECT AddGeometryColumn('ride', 'geom', 4326, 'POINT', 2);
+UPDATE ride SET geom = ST_SetSRID(ST_MakePoint(lon, lat), 4326);
 
 alter table ride
-drop column id,
-drop column point,
-drop column base;
+drop column id;
 
 CREATE INDEX ride_idx
   ON ride
-  USING GIST (geog);
+  USING GIST (geom);
